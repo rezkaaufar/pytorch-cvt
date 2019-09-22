b7efa7431ba87e5307212cf511330bdf85d0ae5f
Rezka Leonandya <rezka.aufar@gmail.com>
2019-09-22 10:40:15 +0200

    try tests

---
+++ tests/cvt-tests.py
@@ -0,0 +1,18 @@
+import itertools
+import math
+import random
+
+from pytest import approx
+import pytest
+import torch
+import torch.nn as nn
+
+from src.encoder import Encoder
+
+class TestEncoder():
+    def test_minimal(self):
+        num_words, num_tags, num_chars = 10, 10, 100
+        encoder = Encoder(num_words, num_tags, num_chars=num_chars)
+
+        assert encoder.num_tags == num_tags
+        assert isinstance(encoder.word_embedding, nn.Embedding)
