407a624570c7ba14e68ae0c4480ea0b05ca1dcda
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-03 07:39:31 +0000

    Adding tarfile member sanitization to extractall()

--- src/artifacts_manager.py
+++ src/artifacts_manager.py
@@ -82,7 +82,26 @@ class ArtifactsManager(object):
             with tarfile.open(self.artifacts_path, 'r:gz') as f:
                 members = [member for member in f.getmembers()
                            if member.name in artifact_names]
-                f.extractall(tmpdirname, members=members)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, tmpdirname, members=members)
 
             self.logger.info('Loading model metadata')
             with open(os.path.join(tmpdirname, self.MODEL_METADATA_FILENAME)) as fm:
